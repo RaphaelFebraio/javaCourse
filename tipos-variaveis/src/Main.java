c43d25ac2323e0519bb719a06d3388468cc3214d
Lumaru <raphaelfebraio96@gmail.com>
2024-03-17 17:54:35 -0300

    exemplos de variaveis

--- tipos-variaveis/src/Main.java
+++ tipos-variaveis/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "string";
+        char chr = 1;
+        int i  = 2;
+        double db = 2.5;
+        long lg = i*10*(100000);
+        boolean tr = true;
+
+        String srt2 = "And many more.";
+
+        System.out.println(str + chr + i + db + lg + tr);
+
+
+        //final não pode ser alterado
+        final double PI = 3.14;
+
     }
 }
